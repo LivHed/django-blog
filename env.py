5801dbe4970add1aca328dfff94e0f20abefee4e
Ubuntu <ubuntu@ip-172-31-88-69.ec2.internal>
2019-12-09 12:29:29 +0000

    add database_url for local testing

--- env.py
+++ env.py
@@ -1,3 +1,5 @@
 import os
 
 os.environ.setdefault("SECRET_KEY", '-g*pzbh6o%f=&_&i&j!_gz1qd4*24katell=l$s7yhz*hx9wpq')
+
+os.environ.setdefault("DATABASE_URL", 'postgres://xssblkpmqbgdfm:cc08f0b776f126bfbd8dfe84fdc941c50fa3d5e98993a5dfa7308f3575aa5f78@ec2-54-247-171-30.eu-west-1.compute.amazonaws.com:5432/ddv6ilaifl4i97')
